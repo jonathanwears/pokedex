a82aa8a105912fa04d74ede749926a84433c5fbc
jonathan <jonathanwears@outlook.com>
2021-07-30 09:38:16 +0200

    fixed a problem with incorrect image link

--- src/components/PokemonImg.jsx
+++ src/components/PokemonImg.jsx
@@ -4,13 +4,14 @@ import { PokemonIndexContext } from "../hooks/pokemonContext";
 
 function PokemonImg() {
 	const [pokemon] = useContext(PokemonIndexContext);
+	const validatedPokemon = pokemon === 121 ? 'mr-mime' : pokedex[pokemon].name.english.toLowerCase().trim();
 
 	return (
 		<div className="img-container">
 			<img
 				className='pokemonImg'
 				name={pokemon}
-				src={`https://img.pokemondb.net/artwork/large/${pokedex[pokemon].name.english.toLowerCase()}.jpg`}
+				src={`https://img.pokemondb.net/artwork/large/${validatedPokemon}.jpg`}
 				alt={pokemon}
 			>
 			</img>
